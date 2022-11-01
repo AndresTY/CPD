ee5527f131c96a0ffd471c018c9ddf88a8ce9cf4
Andres Ducuara <65153399+AndresTY@users.noreply.github.com>
2022-11-01 09:22:50 -0500

    Update mimikyu.py

--- planets/mimikyu.py
+++ planets/mimikyu.py
@@ -2,8 +2,8 @@ import planets
 import planets_cy
 import timer
 
-py = timer.timer(planets.main, iters=4000000)
-cy = timer.timer(planets_cy.main, iters=4000000)
+py = timer.時間(planets.main, iters=4000000)
+cy = timer.時間(planets_cy.main, iters=4000000)
 
 print(py)
 print(cy)
